a8c416ae86d0a709e797d13c1c993ade55238a15
Noloxe <46321954+Noloxe-Fred@users.noreply.github.com>
2020-04-18 21:05:13 +0200

    Change emojiId RDR

--- gamesList.js
+++ gamesList.js
@@ -2,6 +2,6 @@ module.exports = [
     {name: 'Overwatch', emojiId: '701101685556117515'},
     {name: 'Minecraft', emojiId: '701101487375253556'},
     {name: 'Warzone', emojiId: '701101685556117515'},
-    {name: 'Red-Dead-Redemption', emojiId: '701101685556117515'},
+    {name: 'Red-Dead-Redemption', emojiId: '701146154280157275'},
     {name: 'Apéro', emojiId: '701102542809071737'},
 ];
