61b8254ecaef6817e202692eed712c5803fcba73
Frederic Moreau <noloxe.web@gmail.com>
2020-04-18 18:13:35 +0200

    Add games

--- commands/test.js
+++ commands/test.js
@@ -1,5 +1,6 @@
 exports.run = (client, message, args) => {
  console.log('test', client, message, args)
+  message.channel.send("T'as envie de tester des trucs?");
 };
 
 exports.help = {
--- gamesList.js
+++ gamesList.js
@@ -1,4 +1,7 @@
 module.exports = [
-    {name: 'Overwatch', emojiId: '696381916915433593'},
-    {name: 'Minecraft', emojiId: '696403215305932801'}
+    {name: 'Overwatch', emojiId: '701101685556117515'},
+    {name: 'Minecraft', emojiId: '701101487375253556'},
+    {name: 'Warzone', emojiId: '701101685556117515'},
+    {name: 'Red-Dead-Redemption', emojiId: '701101685556117515'},
+    {name: 'Apéro', emojiId: '701102542809071737'},
 ];
