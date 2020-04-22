884508b11ed2cbd606c079b070fe5cd0b75ea0c8
Noloxe <46321954+Noloxe-Fred@users.noreply.github.com>
2020-04-22 18:33:29 +0200

    Change Games name

--- gamesList.js
+++ gamesList.js
@@ -2,6 +2,6 @@ module.exports = [
     {name: 'Overwatch', emojiId: '678315918492827668'},
     {name: 'Minecraft', emojiId: '701099507705708604'},
     {name: 'Warzone', emojiId: '701100782904410232'},
-    {name: 'Red-Dead-Redemption', emojiId: '702402243391127562'},
-    {name: 'Apéro', emojiId: '702401841723604993'},
+    {name: 'RedDeadRedemption', emojiId: '702402243391127562'},
+    {name: 'Apero', emojiId: '702401841723604993'},
 ];
