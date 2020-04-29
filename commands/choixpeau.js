0adf8dadf58c185fbf9689446894dec33259e2b1
Noloxe <46321954+Noloxe-Fred@users.noreply.github.com>
2020-04-29 20:56:06 +0200

    Change Kmizole right

--- commands/choixpeau.js
+++ commands/choixpeau.js
@@ -1,6 +1,9 @@
 exports.run = (client, message, args) => {
+	if (message.author.id === '227353987991601152') {
+		return message.channel.send('Non, tu ne mérites pas cette commande');
+	}
+	
 	const randomNumber = Math.floor(Math.random() * Math.floor(args.length));
-
 
 	message.channel.send(`Résultat: **${args[randomNumber]}**`);
 };
