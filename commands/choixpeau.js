b8776376a0126a8cc7ccd3a5210d9c389955a596
Frederic Moreau <noloxe.web@gmail.com>
2020-04-30 13:04:36 +0200

    Add emojiId

--- commands/choixpeau.js
+++ commands/choixpeau.js
@@ -1,8 +1,13 @@
 exports.run = (client, message, args) => {
-	const randomNumber = Math.floor(Math.random() * Math.floor(args.length));
+
+	if (message.author.id === '227353987991601152') {
+		return message.channel.send('Non, tu ne mérites pas cette commande toi!')
+	}
+	const choices = args.join(' ').split(',');
+	const randomNumber = Math.floor(Math.random() * Math.floor(choices.length));
 
 
-	message.channel.send(`Résultat: **${args[randomNumber]}**`);
+	message.channel.send(`Résultat: **${choices[randomNumber]}**`);
 };
 
 exports.help = {
