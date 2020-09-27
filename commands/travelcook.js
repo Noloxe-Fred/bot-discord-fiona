b0f65b0b5ff310ad5c86a45cbab44f63001b5455
Marianne <mariannexpiquet@gmail.com>
2020-09-27 23:46:48 +0200

    random éxécuter mais répétition de la commande anormale

--- commands/travelcook.js
+++ commands/travelcook.js
@@ -1,24 +1,26 @@
-const fetch = require('node-fetch');
+const snekfetch = require('snekfetch');
 
-exports.run = (client, message, args) => {
+exports.run = async (client, message, args) => {
+
+const baseURL = "https://restcountries.eu/rest/v2/all";
+const array1 = []
+
+    // Je récupère les données de l'URL
+    await snekfetch.get(baseURL)
+        // puis, Je récupère mon tableau d'objets (r.body) sur lequel je boucle pour récupérer la prop name de chaque objets
+        .then(r => {
+            const arrayObjects = r.body;
+            const array = arrayObjects.map(el => el.name);
+            const random = Math.floor(Math.random() * Math.floor(array.length));  
+               
+            message.channel.send(`C'est parti pour cuisiner à **${array[random]}** !`);
+
+        });
+
+       
+}
 
 // TODO 1 : faire mon appel API pour récupérer les pays de cette dernière 
 // API : https://restcountries.eu/rest/v2/all
 
 // @Link {https://discordjs.guide/additional-info/rest-api.html#making-http-requests-with-node}
-
-const baseURL = "https://restcountries.eu/rest/v2/all";
-
-// L'URL regroupe un tableau d'objets
-// Dans ces objet, on veut récupérer la propriété "name"
-
-// Pour cela, je dois déjà boucler sur le tableau 
-for(object in baseURL){
-    console.log(object.lenght)
-}
-
-const truc = fetch(baseURL).then(response => response.json())
-
-console.log(truc)
-};
-
