0f0b7bee93965777f8339aec5803061f71ff58d9
Marianne <mariannexpiquet@gmail.com>
2020-09-27 23:49:53 +0200

    simplification de la commande : toujours pbm de répétition

--- commands/travelcook.js
+++ commands/travelcook.js
@@ -3,21 +3,18 @@ const snekfetch = require('snekfetch');
 exports.run = async (client, message, args) => {
 
 const baseURL = "https://restcountries.eu/rest/v2/all";
-const array1 = []
 
     // Je récupère les données de l'URL
-    await snekfetch.get(baseURL)
+    snekfetch.get(baseURL)
         // puis, Je récupère mon tableau d'objets (r.body) sur lequel je boucle pour récupérer la prop name de chaque objets
         .then(r => {
             const arrayObjects = r.body;
-            const array = arrayObjects.map(el => el.name);
+            const array = arrayObjects;
             const random = Math.floor(Math.random() * Math.floor(array.length));  
                
-            message.channel.send(`C'est parti pour cuisiner à **${array[random]}** !`);
+            message.channel.send(`C'est parti pour cuisiner à **${array[random].name}** !`);
 
-        });
-
-       
+        });     
 }
 
 // TODO 1 : faire mon appel API pour récupérer les pays de cette dernière 
